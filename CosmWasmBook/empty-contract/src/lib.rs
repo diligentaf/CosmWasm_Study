d3b206aee3be91a283a99c6514d2ee9034480944
moog.kwon <diligentaf@gmail.com>
2022-08-11 16:27:22 +0900

    chapter 3.4 wip

--- CosmWasmBook/empty-contract/src/lib.rs
+++ CosmWasmBook/empty-contract/src/lib.rs
@@ -1,8 +1,33 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+// use cosmwasm_std::{
+//     entry_point, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult,
+// };
+
+// #[entry_point] // raw Wasm entry point
+// pub fn instantiate(
+//     _deps: DepsMut, // querying and updating the contract state, querying other contract state, helper functions for dealing with cw address
+//     _env: Env, // object representing blockchains state when executing the message - chain height, id, current timestamp, contract address
+//     _info: MessageInfo, // metainformation about the message which triggered an execution. address that send the message, chain token sent with message
+//     _msg: Empty, // message triggering execution itself. Argument can be anything that's deserializable
+// ) -> StdResult<Response> {
+//     Ok(Response::new())
+// }
+
+use cosmwasm_std::{
+    entry_point, to_binary, Binary, Deps, DepsMut, Empty, Env, MessageInfo,
+    Response, StdResult,
+};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct QueryResp {
+    message: String,
+}
+
+#[entry_point]
+pub fn query(_deps: Deps, _env: Env, _msg: Empty) -> StdResult<Binary> {
+    let resp = QueryResp {
+        message: "Hello World".to_owned(),
+    };
+
+    to_binary(&resp)
 }
